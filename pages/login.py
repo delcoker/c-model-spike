85c0e017258c3c5fc04541b3f3c7b222ad7043c5
del <kingston.coker@statista.com>
2022-05-30 01:08:46 +0200

    hacky gsheet logging

--- pages/login.py
+++ pages/login.py
@@ -5,15 +5,18 @@ import pandas as pd
 import pygsheets
 import streamlit as st
 
-# # authorization
-# gc = pygsheets.authorize(service_account_env_var='client_x509_cert_url')
-#
-# # Create empty dataframe
-# df = pd.DataFrame()
-#
-# x = random.randint(1, 18)
-# y = random.randint(1, 33)
+# authorization
+try:
+    gc = pygsheets.authorize(service_account_env_var='GDRIVE_API_CREDENTIALS')
+    st.info("Got GC")
+except:
+    gc = pygsheets.authorize(service_file='C:/Users/delco/Downloads/c-model-test-d0c7fbbcfbff.json')
 
+# Create empty dataframe
+df = pd.DataFrame()
+
+x = random.randint(1, 18)
+y = random.randint(1, 33)
 
 var1 = st.empty()
 
@@ -27,17 +30,18 @@ def app():
     if submit_button and password == 'del':
         st.success(f"Welcome {username}")
 
-        # # Create a column
-        # df['name'] = [username + "->" + str(datetime.now())]
-        #
-        # # open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-        # sh = gc.open('spike-test')
-        #
-        # # select the first sheet
-        # wks = sh[0]
-        #
-        # # update the first sheet with df, starting at cell B2.
-        # wks.set_dataframe(df, (y, x))
+        if gc:
+            # Create a column
+            df['name'] = [username + "->" + str(datetime.now())]
+
+            # open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+            sh = gc.open('spike-test')
+
+            # select the first sheet
+            wks = sh[0]
+
+            # update the first sheet with df, starting at cell B2.
+            wks.set_dataframe(df, (y, x))
 
         var1.empty()
 
